b2b085c66291bd9cbb7f870e3bfc1e9ba949ef00
mariobet23440 <bet23440@uvg.edu.gt>
2026-02-05 19:34:54 -0600

    Creación de Archivos - Ejemplo I2C

---
+++ Recursos/EjemploI2C/EjemploI2C/main.c
@@ -0,0 +1,18 @@
+/*
+ * EjemploI2C.c
+ *
+ * Created: 5/02/2026 19:34:34
+ * Author : mario
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
