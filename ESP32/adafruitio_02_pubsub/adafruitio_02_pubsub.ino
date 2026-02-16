95a954df73163650cb28f575c80c4fd278a534f8
mariobet23440 <bet23440@uvg.edu.gt>
2026-02-16 09:47:32 -0600

    HITO - COMUNICACIÓN ADAFRUIT-MAESTRO

--- ESP32/adafruitio_02_pubsub/adafruitio_02_pubsub.ino
+++ ESP32/adafruitio_02_pubsub/adafruitio_02_pubsub.ino
@@ -1,14 +1,4 @@
 // Adafruit IO Publish & Subscribe Example
-//
-// Adafruit invests time and resources providing this open source code.
-// Please support Adafruit and open source hardware by purchasing
-// products from Adafruit!
-//
-// Written by Todd Treece for Adafruit Industries
-// Copyright (c) 2016 Adafruit Industries
-// Licensed under the MIT license.
-//
-// All text above must be included in any redistribution.
 
 /************************** Configuration ***********************************/
 
@@ -22,26 +12,23 @@
 // this int will hold the current count for our sketch
 int count = 0;
 
-// Track time of last published messages and limit feed->save events to once
-// every IO_LOOP_DELAY milliseconds.
-//
-// Because this sketch is publishing AND subscribing, we can't use a long
-// delay() function call in the main loop since that would prevent io.run()
-// from being called often enough to receive all incoming messages.
-//
-// Instead, we can use the millis() function to get the current time in
-// milliseconds and avoid publishing until IO_LOOP_DELAY milliseconds have
-// passed.
-#define IO_LOOP_DELAY 5000
+#define IO_LOOP_DELAY 10000
 unsigned long lastUpdate = 0;
 
 // set up the 'counter' feed
-AdafruitIO_Feed *counter = io.feed("counter");
+AdafruitIO_Feed *counter          = io.feed("counter");
+AdafruitIO_Feed *humedadFeed      = io.feed("humedad");
+AdafruitIO_Feed *temperaturaFeed  = io.feed("temp");
+AdafruitIO_Feed *motorDCFeed      = io.feed("motordc");
+AdafruitIO_Feed *comandoFeed      = io.feed("comando");
 
 void setup() {
 
   // start the serial connection
   Serial.begin(115200);
+
+  // 1. Arrancamos el Serial 2 (ajusta los baudios según lo que necesites)
+  Serial2.begin(9600, SERIAL_8N1, 16, 17);
 
   // wait for serial monitor to open
   while(! Serial);
@@ -56,6 +43,11 @@ void setup() {
   // will be called whenever a message is
   // received from adafruit io.
   counter->onMessage(handleMessage);
+
+  // 1. Vinculamos el feed "motorDCFeed" con una función de manejo
+  motorDCFeed->onMessage(handleMotor);
+
+  comandoFeed->onMessage(handleComando);
 
   // wait for a connection
   while(io.status() < AIO_CONNECTED) {
@@ -84,6 +76,9 @@ void loop() {
     // Enviamos el número al feed
     counter->save(miNumero);
 
+    temperaturaFeed -> save(miNumero);
+    humedadFeed -> save(miNumero);
+
     lastUpdate = millis();
   }
 }
@@ -96,4 +91,39 @@ void handleMessage(AdafruitIO_Data *data) {
   Serial.print("received <- ");
   Serial.println(data->value());
 
+}
+
+void handleMotor(AdafruitIO_Data *data) {
+  
+  Serial.print("Botón recibido -> ");
+  Serial.println(data->value()); // Imprime "1" o "0"
+
+  // Convertimos el dato a entero para usarlo fácilmente
+  int estado = data->toInt();
+
+  if (estado == 1) {
+    Serial.println("¡Encendiendo Motor!");
+    // digitalWrite(PIN_MOTOR, HIGH); // Aquí podrías activar un pin real
+  } else {
+    Serial.println("Apagando Motor...");
+    // digitalWrite(PIN_MOTOR, LOW);
+  }
+}
+
+void handleComando(AdafruitIO_Data *data) {
+  // Extraemos el valor como texto
+  String valor = data->value(); 
+
+  Serial.print("Comando recibido: ");
+  Serial.println(valor);
+
+  // Comparamos con comillas DOBLES porque es un String
+  if (valor == "A") {
+    Serial.println("Acción para A: Enviando al Serial 2");
+    Serial2.print('A'); 
+  } 
+  else if (valor == "B") {
+    Serial.println("Acción para B: Enviando al Serial 2");
+    Serial2.print('B');
+  }
 }
