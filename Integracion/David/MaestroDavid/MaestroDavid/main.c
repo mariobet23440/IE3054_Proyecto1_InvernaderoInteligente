37edd30aa4819ed8e86168b048be04fcaaa442bd
mariobet23440 <bet23440@uvg.edu.gt>
2026-02-17 20:12:46 -0600

    REVISIÓN MAESTRO DAVID - Factorización
    Construir una función de envío de comandos genérica, eliminar líneas de código repetidas

--- Integracion/David/MaestroDavid/MaestroDavid/main.c
+++ Integracion/David/MaestroDavid/MaestroDavid/main.c
@@ -68,6 +68,14 @@ volatile char rx_buffer[RX_BUFFER_SIZE];
 volatile uint8_t rx_index = 0;
 volatile uint8_t command_ready = 0;
 
+// PROTOTIPOS DE FUNCIONES
+// Procesamiento de comandos
+void Process_Command(void);
+void Fan_SetSpeed(uint8_t speed);
+uint8_t Read_Soil(void);
+
+void I2C_SendCommand(uint8_t slave_addr, uint8_t command);
+
 /************************************************************************/
 /* LCD 4 BITS                                                                    */
 /************************************************************************/
@@ -80,230 +88,6 @@ LCD_4b lcd = {
 	.d6 = { &PORTD, &DDRD, PORTD7 },
 	.d7 = { &PORTB, &DDRB, PORTB0 }
 };
-
-/************************************************************************/
-/* FUNCIONES I2C                                                        */
-/************************************************************************/
-
-// void TSL2561_Init_Sensor(void) {
-// 	if (I2C_MasterStart()) {
-// 		if (I2C_Master_Write((TSL2561_ADDR << 1) | I2C_WRITE) == 0x18) {
-// 			I2C_Master_Write(CMD_POWER_UP);
-// 			I2C_Master_Write(0x03); // Encender
-// 		}
-// 		I2C_MasterStop();
-// 	}
-// }
-//
-// uint16_t TSL2561_Read_Luminosity(void) {
-// 	uint8_t lo = 0, hi = 0;
-// 	if (I2C_MasterStart()) {
-// 		I2C_Master_Write((TSL2561_ADDR << 1) | I2C_WRITE);
-// 		I2C_Master_Write(CMD_READ_DATA);
-// 		I2C_MasterRepeatedStart();
-// 		I2C_Master_Write((TSL2561_ADDR << 1) | I2C_READ);
-// 		I2C_MasterRead(&lo, I2C_ACK);
-// 		I2C_MasterRead(&hi, I2C_NACK);
-// 		I2C_MasterStop();
-// 	}
-// 	return (uint16_t)((hi << 8) | lo);
-// }
-
-
-// NOTAS PARA RENOVACIÓN : Crear una función SendCommand
-// Evitar líneas repetidas
-// Encender bomba de riego
-void Pump_Start_Command(void)
-{
-	// Escribir comando en esclavo SLAVE_ACTUACTORS
-	I2C_MasterStart();
-	I2C_Master_Write((SLAVE_ACTUATORS_ADDR<<1)|I2C_WRITE);
-	I2C_Master_Write(CMD_PUMP_START);
-	I2C_MasterStop();
-}
-
-// Apagar bomba de riego
-void Pump_Stop_Command(void)
-{
-	// Escribir comando en esclavo SLAVE_ACTUACTORS
-	I2C_MasterStart();
-	I2C_Master_Write((SLAVE_ACTUATORS_ADDR<<1)|I2C_WRITE);
-	I2C_Master_Write(CMD_PUMP_STOP);
-	I2C_MasterStop();
-}
-
-// Abrir puerta (servomotor)
-void Servo_Open(void)
-{
-	// Escribir comando en SLAVE_ACTUATORS
-	I2C_MasterStart();
-	I2C_Master_Write((SLAVE_ACTUATORS_ADDR<<1)|I2C_WRITE);
-	I2C_Master_Write(CMD_SERVO_OPEN);
-	I2C_MasterStop();
-}
-
-// Cerrar puerta (servomotor)
-void Servo_Close(void)
-{
-	// Escribir comando en SLAVE_ACTUATORS
-	I2C_MasterStart();
-	I2C_Master_Write((SLAVE_ACTUATORS_ADDR<<1)|I2C_WRITE);
-	I2C_Master_Write(CMD_SERVO_CLOSE);
-	I2C_MasterStop();
-}
-
-// Encender ventilador (motor DC)
-void Fan_On(void)
-{
-	// Escribir comando en esclavo
-	I2C_MasterStart();
-	I2C_Master_Write((SLAVE_ENV_ADDR<<1)|I2C_WRITE);
-	I2C_Master_Write(CMD_FAN_ON);
-	I2C_MasterStop();
-}
-
-// Apagar ventilador (motor DC)
-void Fan_Off(void)
-{
-	// Escribir comando en esclavo
-	I2C_MasterStart();
-	I2C_Master_Write((SLAVE_ENV_ADDR<<1)|I2C_WRITE);
-	I2C_Master_Write(CMD_FAN_OFF);
-	I2C_MasterStop();
-}
-
-// Establecer velocidad de ventilador
-void Fan_SetSpeed(uint8_t speed)
-{
-	I2C_MasterStart();
-	I2C_Master_Write((SLAVE_ENV_ADDR<<1)|I2C_WRITE);
-	I2C_Master_Write(CMD_FAN_PWM);
-	I2C_Master_Write(speed);
-	I2C_MasterStop();
-}
-
-// Leer sensor de humedad de suelo
-uint8_t Read_Soil(void)
-{
-	uint8_t soil_value = 0;
-
-	if(I2C_MasterStart())
-	{
-		I2C_Master_Write((SLAVE_ENV_ADDR<<1)|I2C_WRITE);
-		I2C_Master_Write(CMD_READ_SOIL);
-
-		I2C_MasterRepeatedStart();
-		I2C_Master_Write((SLAVE_ENV_ADDR<<1)|I2C_READ);
-		I2C_MasterRead(&soil_value, I2C_NACK);
-
-		I2C_MasterStop();
-	}
-
-	return soil_value;
-}
-
-
-
-/************************************************************************/
-/* fUNCIONES AUXILIARES                                                 */
-/************************************************************************/
-// Procesar comandos recibidos por UART
-void Process_Command(void)
-{
-	// Protocolo
-	// P1 -> Start bomba
-	// P0 -> Stop bomba
-	// S1 -> Servo abrir
-	// S0 -> Servo cerrar
-	// S1 -> Servo abrir
-	// S0 -> Servo cerrar
-	// Q  -> Sensor humedad suelo
-	// L  -> Sensor luz ambiente
-	
-	// -- CONSULTAR HUMEDAD DE SUELO ('Q') --
-	if(rx_buffer[0] == 'Q')
-	{
-		// Lectura de humedad de suelo (Byte)
-		uint8_t soil = Read_Soil();
-
-		// Cálculo de porcentaje relativo de humedad
-		uint8_t percentage;
-		
-		// Truncamiento de valor y cálculo en región de sensado
-		if(soil >= SOIL_RAW_DRY) percentage = 0;
-		else if(soil <= SOIL_RAW_WET) percentage = 100;
-		else percentage = 100 - ((soil - SOIL_RAW_WET) * 100) / (SOIL_RAW_DRY - SOIL_RAW_WET);
-		
-		// LOGS SERIALES (Eliminar en implementación)
-		char buffer[40];
-		sprintf(buffer, "Soil: %d (%d%%)\r\n", soil, percentage);
-		UART_SendString(buffer);
-
-		if(percentage > 40) UART_SendString("No necesita agua\r\n");
-		else UART_SendString("Necesita agua\r\n");
-	}
-	
-	// 	else if(rx_buffer[0] == 'L')
-	// 	{ // Comando Luminosidad
-	// 		char buffer[40]
-	// 		uint16_t lux = TSL2561_Read_Luminosity();
-	// 		sprintf(buffer, "Luminosidad: %u lux\r\n", lux);
-	// 		UART_SendString(buffer);
-	// 	}
-	
-	// -- MOTOR STEPPER : BOMBA DE RIEGO --
-	else if(rx_buffer[0] == 'P')
-	{
-		if(rx_buffer[1] == '1'){
-			Pump_Start_Command();				// Encender bomba
-			UART_SendString("Pump START\r\n");	// Eliminar en implementación
-		}
-		else if(rx_buffer[1] == '0'){
-			Pump_Stop_Command();				// Apagar bomba
-			UART_SendString("Pump STOP\r\n");	// Eliminar en implementación
-		}
-	}
-	
-	// -- SERVOMOTOR : Abrir puerta --
-	else if(rx_buffer[0] == 'S')
-	{
-		if(rx_buffer[1] == '1'){
-			Servo_Open();						// Abrir puerta
-			UART_SendString("Servo OPEN\r\n");	// Eliminar en implementación
-		}
-		else if(rx_buffer[1] == '0'){
-			Servo_Close();						// Cerrar puerta
-			UART_SendString("Servo CLOSE\r\n");	// Eliminar en implementación
-		}
-	}
-	
-	// -- MOTOR DC : VENTILADOR --
-	else if(rx_buffer[0] == 'F')
-	{
-		if(rx_buffer[1] == '1'){
-			Fan_On();							// Encender ventilador
-			UART_SendString("Fan ON\r\n");		// Eliminar en implementación
-		}
-		else if(rx_buffer[1] == '0'){
-			Fan_Off();							// Apagar ventilador
-			UART_SendString("Fan OFF\r\n");		// Eliminar en implementación
-		}
-	}
-	
-	// -- MOTOR DC : CONTROL DE VELOCIDAD --
-	else if(rx_buffer[0] == 'V')
-	{
-		uint8_t speed = atoi((char*)&rx_buffer[1]);
-		if(speed > 255) speed = 255;
-
-		Fan_SetSpeed(speed);
-
-		char buffer[40];
-		sprintf(buffer, "Fan speed: %d\r\n", speed);
-		UART_SendString(buffer);
-	}
-
-}
 
 
 /************************************************************************/
@@ -402,4 +186,167 @@ int main(void)
 		_delay_ms(1);
 	}
 }
+
+/************************************************************************/
+/* fUNCIONES AUXILIARES                                                 */
+/************************************************************************/
+// Procesar comandos recibidos por UART
+void Process_Command(void)
+{
+	// Protocolo
+	// P1 -> Start bomba
+	// P0 -> Stop bomba
+	// S1 -> Servo abrir
+	// S0 -> Servo cerrar
+	// S1 -> Servo abrir
+	// S0 -> Servo cerrar
+	// Q  -> Sensor humedad suelo
+	// L  -> Sensor luz ambiente
+	
+	// -- CONSULTAR HUMEDAD DE SUELO ('Q') --
+	if(rx_buffer[0] == 'Q')
+	{
+		// Lectura de humedad de suelo (Byte)
+		uint8_t soil = Read_Soil();
+
+		// Cálculo de porcentaje relativo de humedad
+		uint8_t percentage;
+		
+		// Truncamiento de valor y cálculo en región de sensado
+		if(soil >= SOIL_RAW_DRY) percentage = 0;
+		else if(soil <= SOIL_RAW_WET) percentage = 100;
+		else percentage = 100 - ((soil - SOIL_RAW_WET) * 100) / (SOIL_RAW_DRY - SOIL_RAW_WET);
+		
+		// LOGS SERIALES (Eliminar en implementación)
+		char buffer[40];
+		sprintf(buffer, "Soil: %d (%d%%)\r\n", soil, percentage);
+		UART_SendString(buffer);
+
+		if(percentage > 40) UART_SendString("No necesita agua\r\n");
+		else UART_SendString("Necesita agua\r\n");
+	}
+	
+	// 	else if(rx_buffer[0] == 'L')
+	// 	{ // Comando Luminosidad
+	// 		char buffer[40]
+	// 		uint16_t lux = TSL2561_Read_Luminosity();
+	// 		sprintf(buffer, "Luminosidad: %u lux\r\n", lux);
+	// 		UART_SendString(buffer);
+	// 	}
+	
+	// -- MOTOR STEPPER : BOMBA DE RIEGO --
+	else if(rx_buffer[0] == 'P')
+	{
+		if(rx_buffer[1] == '1'){
+			I2C_SendCommand(SLAVE_ACTUATORS_ADDR, CMD_PUMP_START);				// Encender bomba
+			UART_SendString("Pump START\r\n");	// Eliminar en implementación
+		}
+		else if(rx_buffer[1] == '0'){
+			I2C_SendCommand(SLAVE_ACTUATORS_ADDR, CMD_PUMP_STOP);				// Apagar bomba
+			UART_SendString("Pump STOP\r\n");	// Eliminar en implementación
+		}
+	}
+	
+	// -- SERVOMOTOR : Abrir puerta --
+	else if(rx_buffer[0] == 'S')
+	{
+		if(rx_buffer[1] == '1'){
+			I2C_SendCommand(SLAVE_ACTUATORS_ADDR, CMD_SERVO_OPEN);						// Abrir puerta
+			UART_SendString("Servo OPEN\r\n");	// Eliminar en implementación
+		}
+		else if(rx_buffer[1] == '0'){
+			I2C_SendCommand(SLAVE_ACTUATORS_ADDR, CMD_SERVO_CLOSE);						// Cerrar puerta
+			UART_SendString("Servo CLOSE\r\n");	// Eliminar en implementación
+		}
+	}
+	
+	// -- MOTOR DC : VENTILADOR --
+	else if(rx_buffer[0] == 'F')
+	{
+		if(rx_buffer[1] == '1'){
+			I2C_SendCommand(SLAVE_ENV_ADDR, CMD_FAN_ON);							// Encender ventilador
+			UART_SendString("Fan ON\r\n");		// Eliminar en implementación
+		}
+		else if(rx_buffer[1] == '0'){
+			I2C_SendCommand(SLAVE_ENV_ADDR, CMD_FAN_OFF);							// Apagar ventilador
+			UART_SendString("Fan OFF\r\n");		// Eliminar en implementación
+		}
+	}
+	
+	// -- MOTOR DC : CONTROL DE VELOCIDAD --
+	else if(rx_buffer[0] == 'V')
+	{
+		uint8_t speed = atoi((char*)&rx_buffer[1]);
+		if(speed > 255) speed = 255;
+
+		Fan_SetSpeed(speed);
+
+		char buffer[40];
+		sprintf(buffer, "Fan speed: %d\r\n", speed);
+		UART_SendString(buffer);
+	}
+
+}
+
+void I2C_SendCommand(uint8_t slave_addr, uint8_t command) {
+	if (I2C_MasterStart()) {
+		I2C_Master_Write((slave_addr << 1) | I2C_WRITE);
+		I2C_Master_Write(command);
+		I2C_MasterStop();
+	}
+}
+
+
+/************************************************************************/
+/* FUNCIONES I2C                                                        */
+/************************************************************************/
+// Establecer velocidad de ventilador
+void Fan_SetSpeed(uint8_t speed)
+{
+	I2C_MasterStart();
+	I2C_Master_Write((SLAVE_ENV_ADDR<<1)|I2C_WRITE);
+	I2C_Master_Write(CMD_FAN_PWM);
+	I2C_Master_Write(speed);
+	I2C_MasterStop();
+}
+
+// Leer sensor de humedad de suelo
+uint8_t Read_Soil(void) {
+	uint8_t soil_value = 0;
+	if (I2C_MasterStart()) {
+		I2C_Master_Write((SLAVE_ENV_ADDR << 1) | I2C_WRITE);
+		I2C_Master_Write(CMD_READ_SOIL);
+		I2C_MasterRepeatedStart();
+		I2C_Master_Write((SLAVE_ENV_ADDR << 1) | I2C_READ);
+		I2C_MasterRead(&soil_value, I2C_NACK);
+		I2C_MasterStop();
+	}
+	return soil_value;
+}
+
+
+// void TSL2561_Init_Sensor(void) {
+// 	if (I2C_MasterStart()) {
+// 		if (I2C_Master_Write((TSL2561_ADDR << 1) | I2C_WRITE) == 0x18) {
+// 			I2C_Master_Write(CMD_POWER_UP);
+// 			I2C_Master_Write(0x03); // Encender
+// 		}
+// 		I2C_MasterStop();
+// 	}
+// }
+//
+// uint16_t TSL2561_Read_Luminosity(void) {
+// 	uint8_t lo = 0, hi = 0;
+// 	if (I2C_MasterStart()) {
+// 		I2C_Master_Write((TSL2561_ADDR << 1) | I2C_WRITE);
+// 		I2C_Master_Write(CMD_READ_DATA);
+// 		I2C_MasterRepeatedStart();
+// 		I2C_Master_Write((TSL2561_ADDR << 1) | I2C_READ);
+// 		I2C_MasterRead(&lo, I2C_ACK);
+// 		I2C_MasterRead(&hi, I2C_NACK);
+// 		I2C_MasterStop();
+// 	}
+// 	return (uint16_t)((hi << 8) | lo);
+// }
+
 
