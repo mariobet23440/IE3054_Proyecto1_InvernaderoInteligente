60b49e250835ec99ab4091fa44f3ab4a150a4e04
mariobet23440 <bet23440@uvg.edu.gt>
2026-02-18 10:49:46 -0600

    Mario - Integración DHT11

--- Integracion/David/Esclavo1David/Esclavo1David/Esclavo1David/main.c
+++ Integracion/David/Esclavo1David/Esclavo1David/Esclavo1David/main.c
@@ -1,90 +1,87 @@
 /*
  * Esclavo_Sensor_DC.c
- *
- * Created: 15/02/2026 09:55:55 p. m.
- * Author : David Carranza
- * Adición de librería DHT11 : Mario Betancourt
+ * Integración de DHT11 para Invernadero
  */ 
 
 #define F_CPU 16000000UL
 
-/************************************************************************/
-/* LIBRERÍAS                                                            */
-/************************************************************************/
-
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdio.h>
 
 #include "ADC.h"
 #include "I2C_Slave.h"
 #include "dc_motor.h"
 #include "UART.h"
-#include <stdio.h>
+#include "DHT11.h" // Librería del sensor
 
 /************************************************************************/
-/*  CONFIGURACIÓN						                                */
+/* CONFIGURACIÓN                                                       */
 /************************************************************************/
 #define SLAVE_ADDR		0x31
 #define CMD_READ_SOIL	0x40
 #define CMD_FAN_ON		0x41
 #define CMD_FAN_OFF		0x42
 #define CMD_FAN_PWM		0x43
-#define CMD_READ_TEMP	0x44
+#define CMD_READ_DHT	'T'   // Comando solicitado: 0x54
 
-// Pin de
-#define SOIL_POWER_PIN	PB0	 // D8
+#define SOIL_POWER_PIN	PB0	 
 
 extern volatile uint8_t tx_data;
 
-
-/************************************************************************/
-/*                               MAIN                                    */
-/************************************************************************/
+// Variables globales para almacenar la última lectura
+uint8_t humidity = 0;
+uint8_t temperature = 0;
 
 int	main(void)
 {	
-	//uint16_t soil_value;
 	uint8_t command; 
 	
 	// Inicializadores
 	UART_Init(UART_BAUD_9600_16MHZ, UART_INTERRUPTS_DISABLED);
-	UART_SendString("Sensor listo\r\n");
+	UART_SendString("Esclavo Invernadero: Sistema Listo\r\n");
+	
 	I2C_Slave_Init(SLAVE_ADDR);
 	ADC_Init();
 	DC_Init();
+	DHT11_Init(); // Inicializar DHT11
 	
-	// Configuración de pin de alimentación del sensor
+	// Configuración de pin de alimentación del sensor de suelo
 	DDRB |= (1 << SOIL_POWER_PIN);
-	PORTB &= ~(1 << SOIL_POWER_PIN);	// Apagado inicialmente
+	PORTB &= ~(1 << SOIL_POWER_PIN);
 	
 	while(1)
 	{
 		command = I2C_Slave_CheckCommand();
 		
 		if (command != 0xFF)
-		
 		{
 			switch(command)
 			{
 				case CMD_READ_SOIL:
 				{
-					 uint16_t soil_value;
+					PORTB |= (1 << SOIL_POWER_PIN);
+					_delay_ms(10); // Breve estabilización
+					uint16_t soil_value = ADC_Read(0);
+					PORTB &= ~(1 << SOIL_POWER_PIN);
 					
-					 // Encender sensor
-					 PORTB |= (1 << SOIL_POWER_PIN);
-					 //_delay_ms(50);		// Estabilizar lectura
-			 
-					 soil_value = ADC_Read(0);	// A0
-					 
-					 // Apagar sensor
-					 PORTB &= ~(1 << SOIL_POWER_PIN);
-			 
-					 // Convertir a 8 bits para I2C
-					 uint8_t soil_8bit = soil_value >> 2;
-			 
-					 // Preparar respuesta
-					 tx_data = soil_8bit;
-					 
+					tx_data = (uint8_t)(soil_value >> 2); // Conversión a 8 bits
+					UART_SendString("Lectura Suelo enviada\r\n");
+				}
+				break;
+				
+				case CMD_READ_DHT:
+				{
+					// Intentar leer el sensor
+					if (DHT11_Read(&humidity, &temperature) == 1) {
+						// Para este ejemplo, cargamos la Temperatura en tx_data 
+						// para que el Maestro la reciba de inmediato.
+						tx_data = temperature; 
+						UART_SendString("Lectura DHT11 Exitosa\r\n");
+					} else {
+						tx_data = 0xEE; // Código de error si falla la lectura
+						UART_SendString("Error en DHT11\r\n");
+					}
 				}
 				break;
 				
@@ -97,22 +94,12 @@ int	main(void)
 					break;
 				
 				case CMD_FAN_PWM:
-				// El esclavo debe esperar el siguiente byte que contiene la velocidad
-				// Función para leer el dato que viene después del comando
 				{
-					// Esperar siguiente dato
+					// Esperar el byte de velocidad que envía el maestro
 					while((TWSR & 0xF8) != 0x80);
 					uint8_t speed = TWDR;
-
 					TWCR = (1<<TWEN)|(1<<TWEA)|(1<<TWINT);
-
 					DC_SetSpeed(speed);
-				}
-				break;
-				
-				case CMD_READ_TEMP:
-				{
-					
 				}
 				break;
 			}		 
